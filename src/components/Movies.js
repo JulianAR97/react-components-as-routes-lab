3f100b3162db2da289f8cb255b5f8b814d45e6fe
Julian <julianrosenthal@yahoo.com>
2021-03-29 14:21:12 -0700

    commit tree 64f4f8b04f96a5fd1063ff308285e4b82ac21f1f

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,25 @@
+import { nodeName } from 'jquery';
 import React from 'react';
 import { movies } from '../data';
 
+const hStyle = {
+  padding: '5px',
+  margin: '0px'
+}
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => (
+        <div key={movie.title}>
+          <h4 style={hStyle}>Title: {movie.title}</h4>
+          <h4 style={hStyle}>Length: {movie.time} mins</h4>
+          <h4 style={hStyle}>Genres: </h4>
+          <ul style={{'margin-top': "0px"}}>
+            {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
